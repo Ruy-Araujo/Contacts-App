1eb83e48a26b94859d2da0ef74a7716cf4f6c5c4
Ruy-Araujo <ruy.araujo@yahoo.com.br>
2020-12-04 19:35:28 -0300

    Adicionado validação email e  alteração/exclusão

--- agenda.py
+++ agenda.py
@@ -7,6 +7,7 @@ def novoContato():
     nome     = input('Digite um nome para o contato: ')
     email    = input('Digite um email para o contato: ')
     telefone = input('Digite um telefone para o contato: ')
+    email = validaEmail(email)   
     listaContatos.append(Contato(nome,email,telefone))
     imprimeLista()
 
@@ -20,6 +21,17 @@ def imprimeLista():
         tabela.add_row([contato.nome,contato.email,contato.telefone])
     print(tabela)
 
+def validaEmail(email):
+    valido = False
+    while not valido:
+        resultado = buscaEmail(email)
+        if resultado:
+            print(f'O email "{email}" já está cadastrado')
+            email = input('Digite outro email: ')
+        else:
+            valido = True
+    return email
+
 def buscaNome(nome):
     resultado = ([contato for contato in listaContatos if contato.nome == nome])
     #tablea = PrettyTable(['Nome','Email','Telefone'])
@@ -31,16 +43,39 @@ def buscaNome(nome):
 
 def buscaEmail(email):
     resultado = ([contato for contato in listaContatos if contato.email == email])
+    if not resultado:
+        return False
+    return resultado[0]
+
+def buscaTelefone(telefone):
+    resultado = ([contato for contato in listaContatos if contato.telefone == telefone])
     tabela = tabelaModelo()
     for contato in resultado:
         tabela.add_row([contato.nome,contato.email,contato.telefone])
     print(tabela)
 
-def buscaTelefone(email):
-    resultado = ([contato for contato in listaContatos if contato.email == email])
-    tabela = tabelaModelo()
-    for contato in resultado:
-        tabela.add_row([contato.nome,contato.email,contato.telefone])
-    print(tabela)
+def alteraContato(email):
+    contato = buscaEmail(email)
+    if not contato:
+        print(f'Não existe nenhum contato cadastrado com o email "{email}"')
+        return
+    nome     = input('Digite um novo nome para o contato: ')
+    telefone = input('Digite um novo telefone para o contato: ') 
+    contato.nome,contato.telefone = nome,telefone
 
-#def deletaContato():
+def deletaContato(email):
+    contato = buscaEmail(email)
+    if not contato:
+        print(f'Não existe nenhum contato cadastrado com o email "{email}"')
+        return
+
+    listaContatos.remove(contato)
+
+"""
+novoContato()
+novoContato()
+novoContato()
+alteraContato('m@m')
+deletaContato('a@a')
+imprimeLista()
+"""
